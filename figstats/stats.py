a06513d5c379074569d25e98cbb67ef8fae6e9ea
Chun Ly <astro.chun@gmail.com>
2020-11-18 12:58:05 -0700

    stats.Figshare: Add institution in get_totals #1

--- figstats/stats.py
+++ figstats/stats.py
@@ -4,6 +4,7 @@ from os.path import join
 
 from .commons import issue_request
 
+
 class Figshare:
     """
     Purpose:
@@ -15,7 +16,7 @@ class Figshare:
 
         self.baseurl = 'https://stats.figshare.com'
         self.institution = institution
-        if not institute:
+        if institute:
             self.institute = institute
             self.baseurl_institute = join(self.baseurl, self.institute)
         self.token = token
@@ -29,11 +30,13 @@ class Figshare:
         else:
             return join(self.baseurl, link)
 
-    def get_totals(self, item_id, item='article'):
+    def get_totals(self, item_id, item='article', institution=False):
         total_dict = {}
         for counter in ['views', 'downloads', 'shares']:
             # Using non-institution one since that seems to give correct stats
-            url = self.endpoint(join('total', counter, item, str(item_id)))
+            url = self.endpoint(join('total', counter, item, str(item_id)),
+                                institution=institution)
+            print(url)
             result = issue_request('GET', url, headers=self.headers)
             total_dict[counter] = result['totals']
         return total_dict
